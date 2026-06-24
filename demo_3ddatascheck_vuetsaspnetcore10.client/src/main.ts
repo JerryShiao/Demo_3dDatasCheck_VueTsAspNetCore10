a2470dc8f2b822dedcb5225482a097a2a88738e7
JerryShiao <61370990+JerryShiao@users.noreply.github.com>
2026-06-24 13:11:31 +0800

    擴充 Window 型別

--- demo_3ddatascheck_vuetsaspnetcore10.client/src/main.ts
+++ demo_3ddatascheck_vuetsaspnetcore10.client/src/main.ts
@@ -1,4 +1,4 @@
-;(window as any).CESIUM_BASE_URL = '/node_modules/cesium/Build/Cesium/';
+window.CESIUM_BASE_URL = '/node_modules/cesium/Build/Cesium/';
 import './assets/main.css'
 
 import { createApp } from 'vue'
